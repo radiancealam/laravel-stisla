a2c510022d80f8163cbbd5cd0ca843e80b49ff4d
pratamaradiance7 <pratamaradiance7@gmail.com>
2022-10-31 10:25:55 +0700

    fixing sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,6 @@
 @section('content')
     <div class="section-header">
         <h1>Dashboard</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-        </div>
     </div>
 
     {{-- Card --}}
--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -13,12 +13,52 @@
             </li>
             <li class="menu-header">Menu</li>
             <li class="dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                    <span>Dropdown</span></a>
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-ticket-alt"></i>
+                    <span>Ticket</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="#">Default Layout</a></li>
-                    <li><a class="nav-link" href="#">Transparent Sidebar</a></li>
-                    <li><a class="nav-link" href="#">Top Navigation</a></li>
+                    <li><a class="nav-link" href="#">Manage Ticket</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i>
+                    <span>User</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage User</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-building"></i>
+                    <span>Department</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage Department</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-flag"></i>
+                    <span>Priority</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage Priority</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-clipboard-check"></i>
+                    <span>Status</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage Status</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-code"></i>
+                    <span>Problem Code</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage Problem Code</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-network-wired"></i>
+                    <span>Media</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Manage Media</a></li>
                 </ul>
             </li>
     </aside>
